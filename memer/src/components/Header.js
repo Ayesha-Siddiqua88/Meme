2803ca57367075eebb276b29654f66a6f73ffe30
Ayesha Siddiqua <siddiquaayesha18.2003@gmail.com>
2024-10-31 00:29:38 +0530

    final

--- memer/src/components/Header.js
+++ memer/src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header(){
         
 
         <header className="flex flex-row justify-center gap-2 items-center pt-5 lg:pt-10">
-            <img src="Troll face.png"  className=" w-16 h-14 lg:w-24 lg:h-20" alt="pic"/>
+            <img src="Troll Face.png"  className=" w-16 h-14 lg:w-24 lg:h-20" alt="pic"/>
             <h1 className=" text-2xl font-extrabold lg:text-3xl">Meme Generator</h1>
         </header>
 
